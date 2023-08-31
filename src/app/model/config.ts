ecbf027d9fd6ff2a41085daa76cd28430211a2e7
MichaelBass <bass_michael@hotmail.com>
2023-08-31 15:16:54 -0500

    Config setting

--- src/app/model/config.ts
+++ src/app/model/config.ts
@@ -1,9 +1,3 @@
 export class Config {
-	
   static server = "https://mss.fsm.northwestern.edu";
-  	
-  static apiUrl = "https://mss.fsm.northwestern.edu/redcap/api/index.php";
-  static token = "9EFC9B914A8E3D1D421B5046A1D9587F"; //"F01B3BCAC30EF765D3BA85A6027AE1FA";
-  static apiUrl2 = "https://redcap.nubic.northwestern.edu/redcap/api/";
-  static token2 = "7B8571ABF90DB1CCB614C760443BB4DE";
 }
